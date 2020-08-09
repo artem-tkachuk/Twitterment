170129b7408464c597d89a827d50f46381cf5edd
Artem <atkachuk@uttpic.com>
2020-08-08 21:30:35 -0700

    Initial Commit

---
+++ Twitterment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Twitterment
+//
+//  Created by Artem Tkachuk on 8/8/20.
+//  Copyright © 2020 Artem Tkachuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
